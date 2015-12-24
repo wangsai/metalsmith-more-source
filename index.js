f925940ac1d5153f3db0d15cd0bd4d27a3620ff4
王赛 <wangsai@bootcss.com>
2015-12-24 22:12:25 +0800

    initial funciton

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = plugin;
+
+function plugin(source) {
+
+  return function(files, ms, done) {
+    if(!_.isString(source)) return done(new Error('source path must be a String.'));
+
+    var path = ms.path(source);
+    ms.read(path, function(err, newfiles){
+      if(err) return done(err);
+
+      Object.keys(newfiles).forEach(function(key){
+        files[key] = newfiles[key];
+      });
+
+      done();
+    });
+      
+  };
+}
